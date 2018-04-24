8d4dce22391920ff69973e7dba6ffc83de55c25c
Alexis Tremblay <alexis.tremblay@gmail.com>
2018-04-24 11:17:12 -0400

    Implemented an LSTM with the gates specified in the paper
    The PyTorch implementation is slightly different with regards
    to the biases. Instead of having one bias vector per gates
    they use two, one for the input and the other one for the
    hidden state. To make sure we are doing the same thing as in the
    paper this is a straightforward implementation of the equations
    11 to 15. This is not optimized.

--- model.py
+++ model.py
@@ -2,72 +2,80 @@
 # @Author: Alexis Tremblay
 # @Date:   2018-04-24 08:41:35
 # @Last Modified by:   Alexis Tremblay
-# @Last Modified time: 2018-04-24 08:56:55
+# @Last Modified time: 2018-04-24 11:15:42
 
 
-from torch.nn import Parameter, Variable
+from torch.nn import Parameter
+from torch.autograd import Variable
 import torch.nn as nn
+import torch.nn.functional as F
 import torch
 import numpy as np
+import math
 
 
 class ChronoLSTM(nn.Module):
     """A chrono LSTM implementation"""
-    def __init__(self, num_inputs, hidden_size, num_layers=1, batch_size=32):
+    def __init__(self, input_size, hidden_size, num_layers=1, batch_size=32):
         super(ChronoLSTM, self).__init__()
 
-        self.num_inputs = num_inputs
+        self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.batch_size = 32
+        self.batch_size = batch_size
 
-        self.lstm = nn.LSTM(input_size=num_inputs,
+        # Input gate
+        self.w_xi = Parameter(torch.Tensor(input_size, hidden_size))
+
+        self.w_hi = Parameter(torch.Tensor(hidden_size, hidden_size))
+        self.b_i = Parameter(torch.Tensor(hidden_size)).unsqueeze(0)
+
+        # Forget gate
+        self.w_xf = Parameter(torch.Tensor(input_size, hidden_size))
+        self.w_hf = Parameter(torch.Tensor(hidden_size, hidden_size))
+        self.b_f = Parameter(torch.Tensor(hidden_size)).unsqueeze(0)
+
+        # c gate
+        self.w_xc = Parameter(torch.Tensor(input_size, hidden_size))
+        self.w_hc = Parameter(torch.Tensor(hidden_size, hidden_size))
+        self.b_c = Parameter(torch.Tensor(hidden_size)).unsqueeze(0)
+
+        # Output gate
+        self.w_xo = Parameter(torch.Tensor(input_size, hidden_size))
+        self.w_ho = Parameter(torch.Tensor(hidden_size, hidden_size))
+        self.b_o = Parameter(torch.Tensor(hidden_size)).unsqueeze(0)
+
+        self.lstm = nn.LSTM(input_size=input_size,
                             hidden_size=hidden_size,
                             num_layers=num_layers)
 
         # The hidden state is a learned parameter
-        self.lstm_h_bias = Parameter(
+        self.h = Parameter(
             torch.randn(
-                self.num_layers,
-                1,
                 self.hidden_size
             ) * 0.05
-        )
-        self.lstm_c_bias = Parameter(
+        ).repeat(self.batch_size, 1)
+        self.c = Parameter(
             torch.randn(
-                self.num_layers,
-                1,
                 self.hidden_size
             ) * 0.05
-        )
+        ).repeat(self.batch_size, 1)
 
         self.reset_parameters()
-        self.reset_bias()
-
-    def create_new_state(self):
-        # Dimension: (num_layers, batch, hidden_size)
-        lstm_h = self.lstm_h_bias.clone().repeat(1, self.batch_size, 1)
-        lstm_c = self.lstm_c_bias.clone().repeat(1, self.batch_size, 1)
-        self.previous_state = (lstm_h, lstm_c)
-        return lstm_h, lstm_c
+        self.chrono_bias(input_size)
 
     def reset_parameters(self):
-        for p in self.lstm.parameters():
-            if p.dim() == 1:
-                nn.init.constant(p, 0)
-            else:
-                stdev = 5 / (np.sqrt(self.num_inputs + self.hidden_size))
-                nn.init.uniform(p, -stdev, stdev)
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
 
-    def reset_bias(self):
-        # input-hidden bias of the kth layer (b_ii|b_if|b_ig|b_io),
-        # of shape (4*hidden_size)
-
-        # hidden-hidden bias of the kth layer (b_hi|b_hf|b_hg|b_ho),
-        # of shape (4*hidden_size)
-
-        # TODO initialize the biases
-        pass
+    def chrono_bias(self, T):
+        # Initialize the biases according to section 2 of the paper
+        # b_f ∼ log(𝒰 ([1, 𝑇 − 1]))
+        # b_i = -b_f
+        torch.nn.init.uniform(self.b_f, 1, T - 1)
+        self.b_f.data.apply_(math.log)
+        self.b_i = -self.b_f.clone()
 
     def size(self):
         return self.num_inputs, self.hidden_size
@@ -76,6 +84,31 @@ class ChronoLSTM(nn.Module):
         if x is None:
             x = Variable(torch.zeros(self.batch_size, self.num_inputs))
 
-        x = x.unsqueeze(0)
-        outp, self.previous_state = self.lstm(x, self.previous_state)
-        return outp.squeeze(0), self.previous_state
+        (h, c) = self.h, self.c
+        # Input gate
+        i = F.sigmoid(
+            torch.mm(x, self.w_xi) + torch.mm(h, self.w_hi) + self.b_i
+        )
+
+        # Forget gate
+        f = F.sigmoid(
+            torch.mm(x, self.w_xf) + torch.mm(h, self.w_hf) + self.b_f
+        )
+
+        # c gate
+        self.c *= f
+        self.c += i * F.tanh(
+            torch.mm(x, self.w_xc) + torch.mm(h, self.w_hc) + self.b_c
+        )
+
+        # Output gate
+        o = F.sigmoid(
+            torch.mm(x, self.w_xo) + torch.mm(h, self.w_ho) + self.b_o
+        )
+
+        # Hidden state
+        self.h = o * F.tanh(c)
+
+        # Current state
+        state = (self.h, self.c)
+        return h, state
