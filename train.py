fixed the loss computation

train.py
@@ -55,13 +55,11 @@ def train_batch(net, criterion, optimizer, X, Y, task):
     # New sequence
     # net.init_sequence(batch_size)
 
+    loss = Variable(torch.zeros(1))
     if task == 'warpTask':
-        loss = 0
         net.create_new_state()
-        y_out = Variable(torch.zeros(outp_seq_len, batch_size, num_features))
         for i in range(inp_seq_len):
-            y_out[i], _ = net(X[i])
-            loss += criterion(y_out[i], Y[i])
+            loss += criterion(net(X[i])[0], Y[i])
     else:
         # Feed the sequence + delimiter
         for i in range(inp_seq_len):
@@ -73,11 +71,10 @@ def train_batch(net, criterion, optimizer, X, Y, task):
             y_out[i], _ = net()
 
         if net.multi_target:
-            loss = 0
             for y_i, Y_i in zip(y_out, Y):
                 loss += criterion(y_i, Y_i.squeeze())
         else:
-            loss = criterion(y_out, Y)
+            loss += criterion(y_out, Y)
 
     loss.backward()
     optimizer.step()