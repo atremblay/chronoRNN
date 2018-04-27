4e4ef5e6b5e09a5eeaadf851cbcf2fe1c907ac01
Jules Gagnon-Marchand <jules.gagnonm.alt@gmail.com>
2018-04-27 19:54:12 -0400

    cleaned tests up a bit

--- tests.py
+++ tests.py
@@ -1,22 +1,30 @@
 from main import main, POSSIBLE_MODELS, POSSIBLE_TASKS
 from pathlib import Path
 import datetime
+import logging
+
+NUM_BATCHES = 100
+CHECKPOINT_INTERVAL = NUM_BATCHES // 2
+REPORT_INTERVAL = NUM_BATCHES // 2
+BATCH_SIZE = 4
+LOG_LEVEL = logging.WARNING
 
 folder = Path("test_logs")
 folder.mkdir(exist_ok=True)
-
 timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
-
 with open(folder/f"{timestamp}.log", "w") as f:
     for task in POSSIBLE_TASKS:
         for model in POSSIBLE_MODELS:
             try:
                 main(["--task", task, "-pmodel_type=" + model,
-                  "-pnum_batches=100", "-pbatch_size=2",
-                  "--checkpoint_interval", "50", "--report_interval", "50"])
-                message = f"SUCCESS - Task: {task}, Model: {model}"
+                      f"-pnum_batches={NUM_BATCHES}", f"-pbatch_size={BATCH_SIZE}",
+                      "--checkpoint_interval", str(CHECKPOINT_INTERVAL),
+                      "--report_interval", str(REPORT_INTERVAL),
+                      "--log_level", str(LOG_LEVEL)],)
+                message = f"SUCCESS - Task: {task + ',':10} Model: {model}"
+                print(message)
                 f.write(message + "\n")
             except Exception as err:
-                message = f"FAILED - Task: {task}, Model: {model}, Error: '{vars(err)}'"
+                message = f"FAILED - Task: {task + ',':10} Model: {model}, Error: '{vars(err)}'"
                 print(message + "\n")
-                f.write(f"FAILED - Task: {task}, Model: {model}, Error: '{vars(err)}'")
+                f.write(message)
