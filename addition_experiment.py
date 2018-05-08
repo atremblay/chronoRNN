c9fa661f26125db16762dc5660d8644c6622974f
Jules Gagnon-Marchand <jules.gagnonm.alt@gmail.com>
2018-05-07 20:30:11 -0400

    addition final

--- addition_experiment.py
+++ addition_experiment.py
@@ -1,26 +1,54 @@
-from main import main
+from main import main as train
+from addition_graph import main as graph
+import time
+from pprint import pprint
 import logging
+LOGGER = logging.getLogger(__name__)
 
-for bias in ["", "-pchrono=True "]:
-    lr = 1E-3
-    cmd = (f"--task=addTask --checkpoint_interval=1000 "
-           f"-prmsprop_alpha=.9 -prmsprop_momentum=0 "
-           f"-pseq_len=200 "
-           f"-pnum_batches=6000 "
-           f"-pbatch_size=32 "
-           f"-pmodel_type=LSTM {bias}"
-           f"-prmsprop_lr={lr} "
-           f"--log_level {logging.INFO}"
-           ).split(" ")
-    print(cmd)
-    main(cmd)
 
-    cmd = (f"--task=addTask --checkpoint_interval=0 "
-           f"-prmsprop_alpha=.9 -prmsprop_momentum=0 "
-           f"-pseq_len=750 "
-           f"-pnum_batches=20000 "
-           f"-pbatch_size=32 "
-           f"-pmodel_type=LSTM {bias}"
-           f"-prmsprop_lr={lr}").split(" ")
-    print(cmd)
-    # main(cmd)
+def stringify(dict_):
+    for k, v in dict_.items():
+        dict_[k] = str(v)
+    return dict_
+
+
+def compile_args(args):
+    return [f"--{k}={v}" for k, v in args.items()]
+
+
+def compile_pargs(args):
+    return [f"-p{k}={v}" for k, v in args.items() if not v == "False"]
+
+
+def compile(args, pargs_general, pargs_specific):
+    args = compile_args(stringify(args))
+    pargs_general = compile_pargs(stringify(pargs_general))
+    pargs_specific = compile_pargs(stringify(pargs_specific))
+    return args + pargs_general + pargs_specific
+
+
+def main():
+    seed = int(time.time() * 1000000)
+    for chrono in [False, True]:
+        args = dict(task="addTask", checkpoint_interval=3000,
+                    seed=seed, log_level=logging.INFO)
+        pargs_gen = dict(rmsprop_alpha=0.9,
+                         rmsprop_momentum=0.9,
+                         rmsprop_lr=1E-3,
+                         batch_size=256,
+                         model_type="LSTM",
+                         chrono=chrono)
+        pargs_short = dict(seq_len=200, num_batches=10000)
+        pargs_long = dict(seq_len=750, num_batches=30000)
+        compiled_short = compile(args, pargs_gen, pargs_short)
+        compiled_long = compile(args, pargs_gen, pargs_long)
+        pprint(compiled_short)
+        train(compiled_short)
+        pprint(compiled_long)
+        train(compiled_long)
+
+    graph()
+
+
+if __name__ == "__main__":
+    main()
--- addition_graph.py
+++ addition_graph.py
@@ -4,12 +4,22 @@ from pathlib import Path
 from task.taskManager import get_model
 from collections import defaultdict
 import matplotlib.pyplot as plt
+import numpy as np
+from utils.Variable import maybe_cuda
+
+
+def moving_average(values, window, stride):
+    if window == 0:
+        return values[::stride], np.arange(0, len(values), stride)
+    weights = np.repeat(1.0, window) / window
+    sma = np.convolve(values, weights, 'valid')
+    return sma[::stride], np.arange(0, len(values), stride)
 
 
 def load_checkpoint(checkpoint_path):
     checkpoint_path = Path(checkpoint_path)
     assert checkpoint_path.exists()
-    checkpoint = torch.load(checkpoint_path)
+    checkpoint = torch.load(checkpoint_path, map_location=lambda storage, loc: maybe_cuda(storage))
     task = get_model(checkpoint["arguments"])
     task.net.load_state_dict(checkpoint["model"])
     loss_fn = lambda model, x, y: task.__class__.loss_fn(model, x, y, task.criterion) # we hide the criterion
@@ -28,29 +38,52 @@ def parse_checkpoint_names(path):
     return task, model, modifier, batch, seq_len
 
 
-ddd = defaultdict(lambda: defaultdict(dict))
-for path in Path("saves/").glob("*.pth"):
-    task, model, modifier, batch, seq_len = parse_checkpoint_names(path)
-    cp = load_checkpoint(path)
-    history = cp[4]
-    args = cp[5]
-    ddd[seq_len][modifier][batch] = history
+max_y = {200: 0.25, 750: 0.225}
+max_x = {200: 3000, 750: 30000}
+window_size = {200: 1, 750: 1}
+stride = {200: 20, 750: 60}
+LINEWIDTH = 1.25
+def main():
+    ddd = defaultdict(lambda: defaultdict(dict))
+    for path in Path("saves/").glob("*.pth"):
+        task, model, modifier, batch, seq_len = parse_checkpoint_names(path)
+        cp = load_checkpoint(path)
+        history = cp[4]
+        args = cp[5]
+        ddd[seq_len][modifier][batch] = history
+
+    for k0, v0 in ddd.items():
+        for k1, v1 in v0.items():
+            v0[k1] = max(v1.items(), key=lambda it: it[0])
+
+    for k0, v0 in ddd.items():
+        for k1, v1 in v0.items():
+            print(f"{k0:4} {k1:>9} {v1[0]:7}")
+
+    for k, v in ddd.items():
+        print(f"{str(k) + ':':5} {list(v.keys())}")
+        y0 = v["vanilla"][1]["loss"]
+        y1 = v["chrono"][1]["loss"]
+        y0_soft, x0_soft = moving_average(y0, window=window_size[k], stride=stride[k])
+        y1_soft, x1_soft = moving_average(y1, window=window_size[k], stride=stride[k])
+        plt.plot(np.linspace(0, max(x0_soft), len(x0_soft)), np.repeat(0.167, len(x0_soft)),
+                 "--", label="Memoryless", linewidth=LINEWIDTH * 0.5, color="black")
+        plt.plot(x0_soft, y0_soft, label="Vanilla", linewidth=LINEWIDTH)
+        plt.plot(x1_soft, y1_soft, label=f"Chrono {k}", linewidth=LINEWIDTH)
+        assert len(x0_soft) == len(x1_soft)
+        plt.legend()
+        plt.ylim(0, max_y[k])
+        plt.xlim(0, max_x[k])
+        plt.xlabel("Number of Iterations")
+        plt.ylabel("MSE Loss")
+        yticks = np.arange(6) * 0.05
+        yticks = yticks[yticks <= max_y[k]]
+        labels = yticks
+        labels_str = [f"{np.floor(l * 100) / 100}" for l in labels]
+        plt.yticks(labels, labels_str)
+        plt.savefig(f"add_{k}.png", dpi=300, pad_inches=0, bbox_inches="tight")
+        plt.clf()
 
 
-for k0, v0 in ddd.items():
-    for k1, v1 in v0.items():
-        v0[k1] = max(v1.items(), key=lambda it: it[0])
-
-
-for k0, v0 in ddd.items():
-    for k1, v1 in v0.items():
-        print(f"{k0:4} {k1:>9} {v1[0]:7}")
-
-
-for k, v in ddd.items():
-    print(f"{str(k) + ':':5} {list(v.keys())}")
-    plt.plot(v["vanilla"][1]["loss"], label="vanilla")
-    plt.plot(v["chrono"][1]["loss"], label="chrono")
-    plt.legend()
-    plt.ylim(0, 0.45)
-    plt.show()
+if __name__ == "__main__":
+    main()
